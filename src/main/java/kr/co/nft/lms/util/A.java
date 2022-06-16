359e3874006012225c293f59625e5f03e9b0b0c2
FryCandy <FryCandy@DESKTOP-SC45J2U>
2022-06-16 12:59:32 +0900

    [김득환] 깃허브 초기 셋팅

---
+++ src/main/java/kr/co/nft/lms/util/A.java
@@ -0,0 +1,15 @@
+package kr.co.nft.lms.util;
+
+public class A {
+	public static String A = "\033[1;91m\033[1;40m 김민서 ";
+	public static String Z = "\033[0;101m\033[1;34m 장주현 ";
+	public static String C = "\033[0;107m\033[1;90m 전지웅 ";
+	public static String D = "\033[0;101m\033[1;30m 나재선 ";
+	public static String E = "\033[0;106m\033[1;35m 김득환 ";
+	public static String W = "\033[1;94m\033[1;40m 김예담 ";
+	public static String Q = "\033[0;107m\033[1;94m 서경록 ";
+	public static String S = "\033[0;106m\033[1;31m  차소현 ";
+	
+	public static String R = "\u001B[0m";
+
+}
