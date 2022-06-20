80c814f32a328bf73ae24a0a87b9dd5c3debdcdd
tqfa2 <tqfa2@DESKTOP-KMQKC39>
2022-06-20 17:41:51 +0900

    [나재선, 정주현] 설문조사 페이지, 설문조사 항목리스트 페이지 구현 view.getSurveyQuestionListByPage.jsp view.getSurveyListByPage.jsp controller.SurveyController.java mapper.SurveyMapper.java mapper.SurveyMapper.xml Service.SurveyService.java

---
+++ src/main/java/kr/co/nft/lms/controller/SurveyController.java
@@ -0,0 +1,51 @@
+package kr.co.nft.lms.controller;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import kr.co.nft.lms.service.SurveyService;
+import kr.co.nft.lms.util.A;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+@RequestMapping("/survey")
+public class SurveyController {
+	@Autowired private SurveyService surveyService;
+	@GetMapping("/getSurveyListByPage")
+	public String getSurveyListByPage(Model model,
+			@RequestParam(name = "currentPage",defaultValue="1")int currentPage,
+			@RequestParam(name = "rowPerPage",defaultValue = "10") int rowPerPage) {// 디폴트 값 설정
+		//뷰를 호출시 모델레이어로 부터 반환된 값을 뷰로 보낸다
+		Map<String, Object> returnMap = surveyService.getSurveyListByPage(currentPage, rowPerPage);
+		log.debug(A.D+"[SurveyController.getSurveyListByPage] map : " + returnMap + A.R); // 디버깅
+		
+		model.addAttribute("surveyList", returnMap.get("surveyList")); // value를 object로 넘겨줌
+		model.addAttribute("lastPage", returnMap.get("lastPage"));
+		model.addAttribute("currentPage",currentPage);
+		
+		return "/survey/getSurveyListByPage";
+	}
+	
+	@GetMapping("/getSurveyQuestionListByPage")
+	public String getSurveyQuestionListByPage(Model model,
+			@RequestParam(name = "currentPage",defaultValue="1")int currentPage,
+			@RequestParam(name = "rowPerPage",defaultValue = "10") int rowPerPage) {// 디폴트 값 설정
+		//뷰를 호출시 모델레이어로 부터 반환된 값을 뷰로 보낸다
+		Map<String, Object> returnMap = surveyService.getSurveyListByPage(currentPage, rowPerPage);
+		log.debug(A.D+"[SurveyController.getSurveyQuestionListByPage] map : " + returnMap + A.R); // 디버깅
+		
+		model.addAttribute("SurveyQuestionList", returnMap.get("SurveyQuestionList")); // value를 object로 넘겨줌
+		model.addAttribute("lastPage", returnMap.get("lastPage"));
+		model.addAttribute("currentPage",currentPage);
+		
+		return "/survey/getSurveyQuestionListByPage";
+	}
+	
+}
