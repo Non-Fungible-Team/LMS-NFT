1b347f8feac434a802c1833206b26f64355d2a6a
Ck_SsoH <Ck_SsoH@DESKTOP-VJ06FMD>
2022-06-21 20:24:57 +0900

    [차소현, 김득환] 건의,강의 게시판 컬럼변경 > vo.Suggest vo.LectureBoard

--- src/main/java/kr/co/nft/lms/vo/Suggest.java
+++ src/main/java/kr/co/nft/lms/vo/Suggest.java
@@ -4,8 +4,17 @@ import lombok.Data;
 
 @Data
 public class Suggest {
-	private int boardNo;
-	private int root;
-	private String boardSecret;
-	private String boardStatus;
+	private int suggestNo;
+	private int root; //답변할 게시판 번호
+	private String suggestSecret;
+	private String suggestStatus;
+	
+	private String suggestTitle;
+	private String suggestContent;
+	private String suggestCategory;
+	private String memberId;
+	private String suggestPrivilege;
+	private String suggestBlind;
+	private String suggestCreateDate;
+	private String suggestUpdateDate;
 }
