94b9929b35349f9d9bc15f88faad487b7f4c314b
yedam <yedam@yedamPC>
2022-06-21 12:54:57 +0900

    [김민서,김예담] 강의 컨트롤러,서비스 디버깅 코드 추가 > LectureController, Service

--- src/main/java/kr/co/nft/lms/controller/LectureController.java
+++ src/main/java/kr/co/nft/lms/controller/LectureController.java
@@ -78,11 +78,18 @@ public class LectureController {
 	public String getLectureByPage(Model model
 			,@RequestParam(name = "currentPage",defaultValue = "1") int currentPage  // 디폴트값 설정, 자동형변환
 			,@RequestParam(name = "rowPerPage", defaultValue = "10") int rowPerPage) {
-		Map<String,Object> map = lectureService.getLectureByPage(currentPage, rowPerPage); //강의목록 서비스 호출
+		//디버깅코드
+		log.debug(A.W +"[LectureController.teacher.lecture.getLectureByPage.model] model : " + model +A.R);
+		log.debug(A.W +"[LectureController.teacher.lecture.getLectureByPage.currentPage] currentPage : " +currentPage +A.R);
+		log.debug(A.W +"[LectureController.teacher.lecture.getLectureByPage.rowPerPage] rowPerPage : " +rowPerPage +A.R);
+		
+		Map<String,Object> map = lectureService.getLectureByPage(currentPage, rowPerPage); //강의목록 서비스 호출해서 map객체에 저장
 		 //서비스에 저장된 값 가져오서 모델객체에 저장
-		model.addAttribute("lectureList", map.get("lectureList"));
-		model.addAttribute("currentPage", map.get("currentPage"));
-		model.addAttribute("lastPage",map.get("lastPage") );
+		model.addAttribute("lectureList", map.get("lectureList")); //강의목록
+		model.addAttribute("currentPage", map.get("currentPage")); //현재페이지
+		model.addAttribute("lastPage",map.get("lastPage") ); //마지막페이지
+		//디버깅코드
+		log.debug(A.W +"[LectureController.getLectureByPage.map] map(Service 호출) : " +map +A.R);
 		log.debug(A.W +"[LectureController.getLectureByPage.model] model.lectureList : " +map.get("lectureList") +A.R);
 		log.debug(A.W +"[LectureController.getLectureByPage.model] model.currentPage : " +map.get("currentPage") +A.R);
 		log.debug(A.W +"[LectureController.getLectureByPage.model] model.lastPage : " +map.get("lastPage") +A.R);
