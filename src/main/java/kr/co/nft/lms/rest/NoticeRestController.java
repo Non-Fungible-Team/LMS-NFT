22b7b61fd869b0c03d56ae120ef3828b202cdd1d
Ck_SsoH <Ck_SsoH@DESKTOP-VJ06FMD>
2022-07-08 16:21:24 +0900

    [차소현, 김득환] notice(공지) 파일삭제 ajax로 변경 및 강의게시판 들여쓰기 수정 > modifyNotice.jsp modifyLectureBoard.jsp NoticeController.java NoticeRestController.java

---
+++ src/main/java/kr/co/nft/lms/rest/NoticeRestController.java
@@ -0,0 +1,34 @@
+package kr.co.nft.lms.rest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import kr.co.nft.lms.service.NoticeService;
+import kr.co.nft.lms.util.A;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+public class NoticeRestController {
+	@Autowired NoticeService noticeService;
+	
+	public Map<String, Object> removeNoticeFile(HttpServletRequest request, String noticeFileName, int noticeFileNo, int noticeNo){
+		String path = request.getServletContext().getRealPath("/uploadFile/noticeFile/");
+		log.debug(A.S + "[NoticeRestController.removeNoticeFile] path : "+ path + A.R);
+		log.debug(A.S + "[NoticeRestController.removeNoticeFile.param] noticeFileName : "+ noticeFileName + A.R);
+		log.debug(A.S + "[NoticeRestController.removeNoticeFile.param] noticeFileNo : "+ noticeFileNo + A.R);
+		log.debug(A.S + "[NoticeRestController.removeNoticeFile.param] noticeNo : "+ noticeNo + A.R);
+		
+		int row = noticeService.removeNoticeFile(noticeNo, noticeFileName, noticeFileNo, path);
+		log.debug(A.S + "[NoticeRestController.removeNoticeFile] row : "+ row + A.R);
+		Map<String, Object> map = new HashMap<>();
+		map.put("row", row);
+		
+		return map;
+	}
+}
